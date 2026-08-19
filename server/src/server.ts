15db801c8d61dd379d967209c1c44d74957c5db9
aplicaciones-servired <aplicaciones@gruposervired.com.co>
2026-08-19 09:10:07 -0500

    fix: corregir la dirección de enlace del servidor para que escuche en todas las interfaces

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,8 +9,8 @@ const start = async (): Promise<void> => {
     await sequelize.authenticate();
     console.log('Conexion con la base de datos establecida');
 
-    app.listen(port, () => {
-      console.log(`Servidor ejecutandose en http://localhost:${port}`);
+    app.listen(port, '0.0.0.0', () => {
+      console.log(`Servidor ejecutandose en http://0.0.0.0:${port}`);
     });
   } catch (error) {
     console.error('No se pudo iniciar el servidor', error);
